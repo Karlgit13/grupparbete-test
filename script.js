61e70f4668bddbc2a51d9a2857f5897e0eb7a0fb
Alexander <aleck_johansson@hotmail.com>
2024-10-22 11:11:57 +0200

    onload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    buildDeck();
+}
+
 function buildDeck() {
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let types = ["C", "D", "H", "S"];
@@ -7,6 +11,6 @@ function buildDeck() {
         for (let j = 0; j < values.length; j++) {
             deck.push(values[j] + "-" + types[i]);
         }
-        // console.log(deck)
+        // console.log(deck) Kan prova för att se array med kort
     }
 }
