ff5506303e1c0193b25a1909835a6ddc527ff9d4
robjor88 <robjor88@gmail.com>
2024-10-22 15:09:10 +0200

    2 loopar med shufflad deck

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+
 window.onload = function() {
-    buildDeck();
+    
 }
 
-function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"];
-    deck = [];
+const suits = ["hjärter", "ruter", "klöver", "spader"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
 
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + types[i]);
-        }
-        // console.log(deck) Kan prova för att se array med kort
+function createDeck() {
+  let deck = [];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      deck.push({ value: values[j], suit: suits[i] });
     }
+  }
+  deck = deck.sort(() => Math.random() - 0.5);
+  console.log(deck);
+  return deck;
 }
+
+const myDeck = createDeck();
